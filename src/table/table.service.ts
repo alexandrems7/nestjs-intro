a39852f10cdcf0a3c58fde1e90e8474504a3724d
alexandrems7 <alexandremartins770@gmail.com>
2022-11-27 15:08:35 -0300

    create table endpoin

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@nestjs/common/decorators";
 import { CreateTableDto } from "./dto/create-table.dto";
+import { Table } from "./entities/table.entities";
 @Injectable()
 export class TableService{
+    tables: Table[] = [];
 
     findAll() {
         return 'Buscar todos os livros';
     }
 
     create(createTableDto: CreateTableDto) {
-        return 'cadastrar um livro'
+        const table: Table = {id: 'random_id', ...createTableDto}
+
+        this.tables.push(table)
+        return table;
     }
 }
