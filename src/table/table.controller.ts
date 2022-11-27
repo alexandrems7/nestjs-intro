06b8d52a4135f7dfc691ee093fd15ba391501d25
alexandrems7 <alexandremartins770@gmail.com>
2022-11-27 14:48:15 -0300

    feat(table): add 'create endpoint'

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 @Controller('table')
 export class TableController {
     @Get()
     findAll() {
     return 'Buscar todos os livros';
+    }
+
+    @Post()
+    create(){
+        return 'cadastrar um livro'
     }
 }
